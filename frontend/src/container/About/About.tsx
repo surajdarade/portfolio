b2c7c578271732f1dc51fe6deafe0954bff895f5
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-07-29 09:50:21 +0530

    about change

--- frontend/src/container/About/About.tsx
+++ frontend/src/container/About/About.tsx
@@ -27,7 +27,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Design</span> <br />
+        I Know that <span>Good Functionality</span> <br />
         means <span>Good Business</span>
       </h2>
 
